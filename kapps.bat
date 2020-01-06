a6855eae1bac84c1df3ab78b3e633e329bd6325d
exenza <massimo.berta@gmail.com>
2020-01-06 18:53:41 +0000

    first commit

---
+++ kapps.bat
@@ -0,0 +1,2 @@
+start "" C:\Users\Max\Documents\iRacing\kapps\standings.lnk
+start "" C:\Users\Max\Documents\iRacing\kapps\relatives.lnk
---
+++ kappsdetect.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         kappsdetect
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://127.0.0.1:8185/racing-overlay/?profileName=standing*
+// @grant        window.close
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
+var kappsurl=window.location
+var wtitle=""
+var wwidth=""
+var wheigth=""
+var wtop=0
+var wleft=0
+
+kappsurl=String(kappsurl).split("?")
+kappsurl=String(kappsurl[0])+'?kappsvr&'+String(kappsurl[1])
+
+var overlay = String(window.location).split("standings")
+if(overlay.length > 1){
+    wtitle="Standings"
+    wwidth=1110
+    wheigth=420
+} else {
+    wtitle="Relatives"
+    wwidth=430
+    wheigth=250
+    wtop=480
+}
+
+document.title=wtitle
+document.body.style.background="#000000"
+window.resizeTo(wwidth,wheigth)
